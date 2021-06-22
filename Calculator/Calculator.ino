187506a8625a0b08d4bd0b9b82e4192565aaa3b8
ChangHwan Choi <57865037+Hwan0808@users.noreply.github.com>
2021-06-22 23:17:08 +0900

    시리얼 통신

---
+++ Calculator/Calculator.ino
@@ -0,0 +1,48 @@
+byte  inputByte;
+long  calSum;       
+int   tmpNum;
+void setup()
+{
+   Serial.begin(9600);
+}
+void loop()
+{
+calSum = 0;
+tmpNum = 0;
+if (Serial.available() > 0) { 
+    while (Serial.available() > 0) {
+      inputByte = Serial.read(); 
+      if  (inputByte  == 61)  {
+         Serial.print('='); 
+         delay(5);
+         Serial.println(calSum);
+         delay(5);
+     };
+    if  ((inputByte >= 48) && (inputByte <= 57)) 
+    {    tmpNum  = inputByte - 48;
+         calSum  = tmpNum;
+         Serial.print(tmpNum);
+         delay(5);
+     };
+    if  (inputByte == 45)  
+    {    inputByte = Serial.read(); 
+         tmpNum  = inputByte - 48; 
+         Serial.print('-');  
+         delay(5);
+         Serial.print(tmpNum);
+         delay(5);
+         calSum = calSum - tmpNum; 
+     }
+    if  (inputByte == 43)  {
+         inputByte = Serial.read(); 
+         tmpNum  = inputByte - 48;
+         Serial.print('+');  
+         delay(5);
+         Serial.print(tmpNum); 
+         delay(5);
+         calSum = calSum + tmpNum;
+    }
+  }  //  while 반목문의 끝
+ }   //  if 조건문의 끝
+}
+
